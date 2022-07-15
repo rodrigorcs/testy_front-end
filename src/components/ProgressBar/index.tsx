34879bbc84a8bab91aa6bd0d100c687541c771e2
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2022-07-15 11:14:34 -0300

    Created ProgressBar component

---
+++ src/components/ProgressBar/index.tsx
@@ -0,0 +1,49 @@
+import React, { FC, ReactNode } from "react";
+
+import styled, { css } from "styled-components";
+
+interface ProgressBarProps {
+  filledItems: number;
+  totalItems: number;
+}
+
+const StyledProgressBar: FC<{ children: ReactNode }> = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  height: 0.4em;
+  & > :not(:first-child) {
+    margin-left: ${({ theme }) => theme.spacing.xxsmall};
+  }
+`;
+
+interface ProgressItemProps {
+  isFilled: boolean;
+}
+
+const ProgressItem: FC<ProgressItemProps> = styled("span")<ProgressItemProps>`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.neutral.n500};
+  border-radius: ${({ theme }) => theme.sizing.xxsmall};
+
+  ${({ theme, isFilled }) =>
+    isFilled &&
+    css`
+      background-color: ${theme.colors.secondary.s200};
+    `}
+`;
+
+const ProgressBar: FC<ProgressBarProps> = ({ filledItems, totalItems }) => {
+  let progressItems: boolean[] = [];
+  for (let item = 0; item < totalItems; item++) progressItems = [...progressItems, item <= filledItems - 1];
+
+  return (
+    <StyledProgressBar>
+      {progressItems.map((isFilled, index) => (
+        <ProgressItem isFilled={isFilled} key={index} />
+      ))}
+    </StyledProgressBar>
+  );
+};
+
+export default ProgressBar;
