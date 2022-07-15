8a6e89ef80e2672d25491248f182e382cea2eb11
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2022-07-15 11:14:16 -0300

    Created useFetch hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export function useFetch<T = unknown>(url: string) {
+  const BASE_URL = "https://62d092a0d9bf9f17058b1f09.mockapi.io/testy/api";
+  const [data, setData] = useState<T | null>(null);
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      baseURL: BASE_URL,
+      url,
+    }).then((response) => {
+      setData(response.data);
+    });
+  }, []);
+
+  return { data };
+}
